4b8a465b186b61a30d4b8d53b4928aa6c419f16a
Liam <liam.c.147@gmail.com>
2022-09-20 14:43:59 -0400

    add state functionality, minor html changes

--- javascript/countries.js
+++ javascript/countries.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     let dropdown = $('#countries');
     let stateDropdown = $('#states');
+    let countryDropdown = $('#country_id')
 
     dropdown.empty();
     stateDropdown.empty();
+    countryDropdown.empty();
     
     dropdown.append('<option selected="true" disabled>Choose Country</option>');
     dropdown.prop('selectedIndex', 0);
@@ -11,11 +13,15 @@ $(document).ready(function(){
     stateDropdown.append('<option selected="true" disabled>Choose State</option>');
     stateDropdown.prop('selectedIndex', 0);
 
+    countryDropdown.append('<option selected="true" disabled>Choose Country</option>');
+    countryDropdown.prop('selectedIndex', 0);
+
 
     $.getJSON("https://xc-countries-api.herokuapp.com/api/countries/", function(data){
     console.log(data);
     $.each(data, function (key, entry) {
-        dropdown.append($('<option></option>').attr('id', entry.code).text(entry.name));
+        dropdown.append($('<option></option>').attr('id', entry.code).text(entry.name + ", " + entry.id));
+        countryDropdown.append($('<option></option>').attr('id', entry.id).text(entry.name + ", " + entry.id));
         })
     });
 
@@ -33,6 +39,7 @@ $(document).ready(function(){
             });
     })
 
+    //add country form functionality
     $( "#countryForm" ).submit(function(e) {
         e.preventDefault();
         var countryCode = $('#ccode').val();
@@ -56,4 +63,30 @@ $(document).ready(function(){
 
         xhr.send(data);
       });
+
+      //add state functionality
+      $('#stateForm').submit( function(e){
+        e.preventDefault();
+
+        var stateCode = $('#scode').val();
+        var stateName = $('#sname').val();
+        var countryId = $('#country_id option:selected').attr('id');
+        console.log(countryId);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://xc-countries-api.herokuapp.com/api/states/");
+
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onload = () => console.log(xhr.responseText);
+
+        let data = `{
+        "code": "${stateCode}",
+        "name": "${stateName}",
+        "countryId": "${countryId}"
+        }`;
+
+        xhr.send(data);
+      })
   });
